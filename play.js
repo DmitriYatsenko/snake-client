31f8f7355b6dab951476a2bb6e5b4faf15e37214
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-01 23:34:47 +0000

    snake

--- play.js
+++ play.js
@@ -3,17 +3,18 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
     const conn = net.createConnection({
-        host: 165.227.47.243,
+        host: "165.227.47.243",
         port: 50541,
     });
 
     // interpret incoming data as text
     conn.setEncoding("utf8");
 
+    conn.on("connect", () => {
+        console.log("Connecting ...");
+    });
+
     return conn;
 };
 
-conn.on("connect", () => {
-    console.log("Connecting ...");
-});
 connect();
