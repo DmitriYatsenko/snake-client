1526a33bf4a8a6958e80c4a014d0d3d95778bbad
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-02 02:00:06 +0000

    snake client

---
+++ client.js
@@ -0,0 +1,24 @@
+// establishes a connection with the game server
+const net = require('net');
+const host = "165.227.47.243";
+const port = 50541;
+
+const connect = function () {
+    const conn = net.createConnection({
+        host: host,
+        port: port
+    });
+    conn.setEncoding("utf8");
+
+    conn.on("connect", () => {
+        console.log("Connecting ...");
+    });
+
+    return conn;
+};
+
+module.exports = {
+    host: host, // stores 42 as myNumber
+    port: port, // stores "hello" as myString
+    connect: connect
+};
